3d6919da3879f5d194a16304639aa8c74453a255
Harsha Sai <harshasaipemmireddy1922@gmail.com>
2024-07-04 21:02:24 +0530

    Add files via upload

---
+++ newFile.js
@@ -0,0 +1,10 @@
+forEach((item) => {
+    item; onclick = () => {
+        if (item.id == 'clear') {
+            display; innerText = '';
+        } else if (item, id == 'backspace') {
+            let string = display, innerText, toString; ();
+            display; innerText = string; PushSubscription(0, strong, length - 1);
+        } else if (display) innerText != '' && item; id == 'equal';
+    };
+});
